f10f705d6684dec95beced24c3595c8cdd628c51
Mark-Ammari <mark.ammari@gmail.com>
2020-01-12 21:41:48 -0500

    Updates

---
+++ precache-manifest.1047450f7e2accf6574e976414a790ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a7bd1b8b2f475bb8bf68763e6f60c23",
+    "url": "/channelfinder/index.html"
+  },
+  {
+    "revision": "dad68ef907680a999008",
+    "url": "/channelfinder/static/css/main.b5c2608b.chunk.css"
+  },
+  {
+    "revision": "1b669bd7c145e8444e7b",
+    "url": "/channelfinder/static/js/2.1ee45647.chunk.js"
+  },
+  {
+    "revision": "04b21ccc9109def6e71671ed6f2d794a",
+    "url": "/channelfinder/static/js/2.1ee45647.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dad68ef907680a999008",
+    "url": "/channelfinder/static/js/main.5f42e15d.chunk.js"
+  },
+  {
+    "revision": "c4b879f49f1197895bd8",
+    "url": "/channelfinder/static/js/runtime-main.74d8a658.js"
+  },
+  {
+    "revision": "327651b3a1c7bf082cf8c3d9969cc18d",
+    "url": "/channelfinder/static/media/fubotv.327651b3.svg"
+  },
+  {
+    "revision": "bef99f4fec06bf262ea185b92937109e",
+    "url": "/channelfinder/static/media/github.bef99f4f.svg"
+  },
+  {
+    "revision": "da9c79e231f2fa92be6d4a9cf7da07b0",
+    "url": "/channelfinder/static/media/sourcecode.da9c79e2.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/channelfinder/precache-manifest.1047450f7e2accf6574e976414a790ed.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/channelfinder/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
